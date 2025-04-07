a4cdf69a03cd98e1da63a6ca5e857769520af3a4
aleka07 <alikhan.amirkhan@gmail.com>
2025-04-07 18:00:37 +0500

    Clean up default Vue template components

--- fax-kz-frontend/src/views/HomeView.vue
+++ fax-kz-frontend/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// Импорты стандартных компонентов нам больше не нужны
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="p-4 md:p-8"> <!-- Добавим отступы -->
+    <h1 class="text-2xl font-semibold text-gray-800">
+      Главная страница Fax.kz Cards (MVP)
+    </h1>
+    <p class="mt-2 text-gray-600">
+      Скоро здесь будет что-то полезное!
+    </p>
   </main>
 </template>
+
+<style scoped>
+/* Стили для главной страницы, если понадобятся */
+</style>
